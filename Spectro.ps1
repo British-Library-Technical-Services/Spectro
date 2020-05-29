8f1afe8b5a1fd06aa01e4b9e9a4122722e9b61c7
karljenkins <karl.jenkins@bl.uk>
2020-05-29 18:26:15 +0100

    Added progress bar

--- Spectro.ps1
+++ Spectro.ps1
@@ -1,62 +1,107 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
+Add-Type -assembly System.Windows.Forms
 
-Function Get-AudioDir($audioDirectory="")
-
-{
+Function Get-AudioDir($dir=""){
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
 
     $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
     $foldername.Description = "Select Folder Containing Audio Files"
     $foldername.rootfolder = "MyComputer"
-    $foldername.SelectedPath = $audioDirectory
+    $foldername.SelectedPath = $dir
 
         if($foldername.ShowDialog() -eq "OK")
             {
             $folder += $foldername.SelectedPath
             }
+
     return $folder
 }
-
-<#Function Get-SoxDir($soxdir="")
-{
-   
-        [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
-    
-        $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
-        $foldername.Description = "Select SoX Directory"
-        #$foldername.rootfolder = ""
-        $foldername.SelectedPath = "c:\program files (x86)\"
-    
-        if($foldername.ShowDialog() -eq "OK")
-        {
-            $folder += $foldername.SelectedPath
-        }
-        return $folder
-}
-#>
-<# Function Get-MagickDir($magickdir="")
-{
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
-    
-    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
-    $foldername.Description = "Select ImageMagick Directory"
-    $foldername.rootfolder = ""
-    $foldername.SelectedPath = "c:\program files (x86)\"
-
-    if($foldername.ShowDialog() -eq "OK")
-    {
-        $folder += $foldername.SelectedPath
+function Create-Spectrograms(){
+    #Checks for $null audio directory and throws error
+  
+    if($audioDir -eq $null){
+        Go-Error
     }
-    return $folder
+    #Create a directory named 'Spectrograms' in the Audio Root directory
+    
+    $outDir = New-Item -Force -Path "$audioDir" -Name "Spectrograms" -ItemType "Directory"
+    
+## -- Create The Progress-Bar
+$ObjForm = New-Object System.Windows.Forms.Form
+$ObjForm.Text = "Generating Spectrograms ..."
+$ObjForm.Height = 200
+$ObjForm.Width = 500
+#$ObjForm.BackColor = "White"
+
+$ObjForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+$ObjForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+
+## -- Create The Label
+$ObjLabel = New-Object System.Windows.Forms.Label
+$ObjLabel.Text = "Starting. Please wait ... "
+$ObjLabel.Left = 5
+$ObjLabel.Top = 10
+$ObjLabel.Width = 500 - 20
+$ObjLabel.Height = 20
+#$ObjLabel.Font = "Tahoma"
+## -- Add the label to the Form
+$ObjForm.Controls.Add($ObjLabel)
+
+$PB = New-Object System.Windows.Forms.ProgressBar
+$PB.Name = "PowerShellProgressBar"
+$PB.Value = 0
+$PB.Style="Continuous"
+
+$System_Drawing_Size = New-Object System.Drawing.Size
+$System_Drawing_Size.Width = 500 - 40
+$System_Drawing_Size.Height = 20
+$PB.Size = $System_Drawing_Size
+$PB.Left = 5
+$PB.Top = 50
+$ObjForm.Controls.Add($PB)
+
+## -- Show the Progress-Bar and Start The PowerShell Script
+$ObjForm.Show() | Out-Null
+$ObjForm.Focus() | Out-NUll
+$ObjLabel.Text = "Starting. Please wait ... "
+$ObjForm.Refresh()
+
+    #Create spectrograms of wav files using input settings in $audiodir/spectrograms
+            $audioFiles = Get-ChildItem -Path $audioDir -Filter "*.wav"
+            $i = 1
+            ForEach ($audiofile in $audiofiles){
+                $inFile = $audioFile.FullName
+                $outFile = Join-Path -Path $outDir -ChildPath $audioFile.BaseName
+                $fileName = Split-Path -Path $audiofile -Leaf
+                    if($showtext -eq $true){
+                    $printText = "-t $fileName"
+                    }
+                    else{
+                    $printText = ""
+                    }
+                [int]$perComp = (100 / $audiofiles.count) * $i  
+                $PB.Value = $perComp
+		        $ObjLabel.Text = "Processing $fileName ($percomp% done)"
+		        $ObjForm.Refresh()  
+                $process = & sox $infile -n spectrogram $FFTwindow $zColor $zLimit $zQuant -x 1600 $bw $showHiColor $printFriend $printText -o $outfile".png"
+                $process | Wait-Process
+                $i++
+                }
+            $ObjForm.Close()
+            Exit
 }
-#>
 
-#Get directory containing audio files for processing
-
-$audioDir = Get-AudioDir
-
-# $magickdir = Get-MagickDir
+function Go-Error(){
+    $oReturn=[System.Windows.Forms.MessageBox]::Show("No Audio Directory Selected","Error",[System.Windows.Forms.MessageBoxButtons]::OK) 
+switch ($oReturn){
+    "OK" {
+         Exit
+        } 
+    
+}
+    
+}
 
 #Create Dialog Box Object
 
@@ -65,20 +110,29 @@ $audioDir = Get-AudioDir
     $form.Size = New-Object System.Drawing.Size(450,400)
     $form.StartPosition = 'CenterScreen'
 
+#Browse Button
+
+    $browseButton = New-Object System.Windows.Forms.Button
+    $browseButton.Location = New-Object System.Drawing.Point (10,330)
+    $browseButton.Size = New-Object System.Drawing.Size (75,23)
+    $browseButton.Text = 'Folder...'
+    $browseButton.Add_Click({New-Variable -Name audioDir -Value $(Get-Audiodir) -Scope Script -Force})
+    $form.Controls.Add($browseButton)
+    
 #OK Button
 
     $okButton = New-Object System.Windows.Forms.Button
-    $okButton.Location = New-Object System.Drawing.Point(75,320)
+    $okButton.Location = New-Object System.Drawing.Point(75,330)
     $okButton.Size = New-Object System.Drawing.Size(75,23)
     $okButton.Text = 'OK'
     $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
     $form.AcceptButton = $okButton
     $form.Controls.Add($okButton)
-
+      
 #Cancel Button
 
     $cancelButton = New-Object System.Windows.Forms.Button
-    $cancelButton.Location = New-Object System.Drawing.Point(150,320)
+    $cancelButton.Location = New-Object System.Drawing.Point(150,330)
     $cancelButton.Size = New-Object System.Drawing.Size(75,23)
     $cancelButton.Text = 'Cancel'
     $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
@@ -101,7 +155,7 @@ $audioDir = Get-AudioDir
     $bwCheckBox.Text = "Black and White"
     $bwCheckBox.Checked = $false
     $bwCheckBox.Tabindex = 0
-    $bwCheckBox.Control.Add($bwCheckBox)
+    #$bwCheckBox.Control.Add($bwCheckBox)
     $form.Controls.Add($bwCheckBox)
 
 #Add Title Text to Image
@@ -112,7 +166,7 @@ $audioDir = Get-AudioDir
     $addText.Text = "Add filename to image"
     $addText.Checked = $false
     $addText.Tabindex = 0
-    $addText.Control.Add($addText)
+    #$addText.Control.Add($addText)
     $form.Controls.Add($addText)
 
 #high colour pallette option
@@ -123,7 +177,7 @@ $audioDir = Get-AudioDir
     $hiColor.Text = "High Colour Palette"
     $hiColor.Checked = $false
     $hiColor.Tabindex = 0
-    $hiColor.Control.Add($hiColor)
+    #$hiColor.Control.Add($hiColor)
     $form.Controls.Add($hiColor)
 
 #printer friendly option
@@ -134,15 +188,15 @@ $audioDir = Get-AudioDir
     $printerFriendly.Text = "Printer Friendly (Light Background)"
     $printerFriendly.Checked = $false
     $printerFriendly.Tabindex = 0
-    $printerFriendly.Control.Add($printerFriendly)
+    #$printerFriendly.Control.Add($printerFriendly)
     $form.Controls.Add($printerFriendly)
 
 #Choose window type Han, Hamming or Dolph
         $WindowTypeLabel = New-Object System.Windows.Forms.label
         $WindowTypeLabel.Location = New-Object System.Drawing.Point(10,130)
         $WindowTypeLabel.Size = New-Object System.Drawing.Size(130,15)
-        $WindowTypeLabel.BackColor = "Transparent"
-        $WindowTypeLabel.ForeColor = "black"
+        #$WindowTypeLabel.BackColor = "Transparent"
+        #$WindowTypeLabel.ForeColor = "black"
         $WindowTypeLabel.Text = "Select FFT Window"
         $form.Controls.Add($WindowTypeLabel)
 
@@ -157,8 +211,8 @@ $audioDir = Get-AudioDir
     # Select the default value
     $windowType.SelectedIndex       = 1
     $windowType.location            = New-Object System.Drawing.Point(10,150)
-    $windowType.Font                = 'Microsoft Sans Serif,10'
-    $windowType.Conrol.Add($windowType)
+    #$windowType.Font                = 'Microsoft Sans Serif,10'
+    #$windowType.Conrol.Add($windowType)
     $form.Controls.Add($windowType)
 
 #set z axis colour range
@@ -166,8 +220,8 @@ $audioDir = Get-AudioDir
         $colorRangeLabel = New-Object System.Windows.Forms.label
         $colorRangeLabel.Location = New-Object System.Drawing.Point(10,180)
         $colorRangeLabel.Size = New-Object System.Drawing.Size(350,15)
-        $colorRangeLabel.BackColor = "Transparent"
-        $colorRangeLabel.ForeColor = "black"
+        #$colorRangeLabel.BackColor = "Transparent"
+        #$colorRangeLabel.ForeColor = "black"
         $colorRangeLabel.Text = "Set Z axis colour range in dB (lower values increase contrast)"
         $form.Controls.Add($colorRangeLabel)
 
@@ -183,8 +237,8 @@ $audioDir = Get-AudioDir
         $zLimitLabel = New-Object System.Windows.Forms.label
         $zLimitLabel.Location = New-Object System.Drawing.Point(10,230)
         $zLimitLabel.Size = New-Object System.Drawing.Size(400,20)
-        $zLimitLabel.BackColor = "Transparent"
-        $zLimitLabel.ForeColor = "black"
+        #$zLimitLabel.BackColor = "Transparent"
+        #$zLimitLabel.ForeColor = "black"
         $zLimitLabel.Text = "Set upper limit of Z axis in dBFS (negative numbers increase brightness)"
         $form.Controls.Add($zLimitLabel)
 
@@ -201,8 +255,8 @@ $audioDir = Get-AudioDir
         $zQuantLabel = New-Object System.Windows.Forms.label
         $zQuantLabel.Location = New-Object System.Drawing.Point(10,280)
         $zQuantLabel.Size = New-Object System.Drawing.Size(400,20)
-        $zQuantLabel.BackColor = "Transparent"
-        $zQuantLabel.ForeColor = "black"
+        #$zQuantLabel.BackColor = "Transparent"
+        #$zQuantLabel.ForeColor = "black"
         $zQuantLabel.Text = "Set Z axis quantisation (number of colours to render)"
         $form.Controls.Add($zQuantLabel)
 
@@ -226,8 +280,9 @@ $result = $form.ShowDialog()
 
 if ($result -eq [System.Windows.Forms.DialogResult]::OK)
 {
+       
     $bw = ""
-    $showText = ""
+    $showText = $false
     $showHiColor = ""
     $printFriend = ""
     $windowChoice = $windowType.SelectedIndex
@@ -239,7 +294,7 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK)
             $bw = "-m"
         }
         if($addText.Checked){
-            $showText = "-t $fileName"
+            $showText = $true
         }
         if($hiColor.Checked){
             $showHiColor = "-h"
@@ -258,19 +313,8 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK)
         $zColor = "-z $zRange"
         $zLimit = "-Z $zLimitValue"
         $zQuant = "-q $zQuantValue"
-    }
-    
-#Create a directory named 'Spectrograms' in the Audio Root directory
 
-$outDir = New-Item -Force -Path "$audioDir" -Name "Spectrograms" -ItemType "Directory"
+           Create-Spectrograms
+}
 
-#Create spectrograms of wav files using input settings in $audiodir/spectrograms
 
-    $audioFiles = Get-ChildItem -Path $audioDir -Filter "*.wav"
-        ForEach ($audiofile in $audiofiles){
-            $inFile = $audioFile.FullName
-            $outFile = Join-Path -Path $outDir -ChildPath $audioFile.BaseName
-            $fileName = Split-Path -Path $audiofile -Leaf
-            & sox $infile -n spectrogram $FFTwindow $zColor $zLimit $zQuant $bw $showText $showHiColor $printFriend  -o $outfile".png"
-            
-    }
